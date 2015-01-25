2f1a73526d4f75157a64204166b4192ed936e98b
mohansm <msmdevan@yahoo.com>
2015-01-25 17:47:12 -0500

    Specify a matrix and calculate its inverse
    This function allows the user to specify a matrix and calculate its
    inverse. If the inverse already exists, the inverse is returned from
    cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions allow you to define a matrix, x, and calculate 
+## its inverse.  If the inverse already exists, the inverse is called 
+## cache. 
+##
+## The first function, makeCacheMatrrix, allows you to set the values of a matrix, get a previously defined matrix
+## and get and set an inverse of this matrix, if it can be 
+## successfully inverted. 
 
-## Write a short comment describing this function
+makeCacheMatrix<- function(x=matrix()) {
+    inverse<- NULL
+    set<-function(y) {
+        x<<- y
+        inverse<<- NULL
+    }
+    get<- function() x
+    setinverse<- function(solve) inverse<<- solve
+    getinverse<- function() inverse
+    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+}
+## The second function,cacheSolve, calcualtes the inverse of a matrix, if it has 
+## not been already calculated before. If it has been calcualted before,
+## it retrieves the inverse from cache. 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve<- function(x,...) {
+    ## Return a matrix that is the inverse of 'x'
+    inverse<-x$getinverse()
+    if (!is.null(inverse)) {
+        message("getting cached value for inverse")
+        return(inverse)
+    }
+    data<-x$get()
+    inverse<- solve(data,...)
+    x$setinverse(inverse)
+    inverse
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
